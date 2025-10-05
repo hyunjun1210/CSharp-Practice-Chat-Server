3ea5fcc42dfa9cd8fa62cdbe9f051d1229c92b5b
현준 <117495886+hyunjun1210@users.noreply.github.com>
2025-10-05 15:53:23 +0900

    Test Server

---
+++ ServerTest/Client/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint end = new IPEndPoint(ipAddr, 7777);
+
+            using (TcpClient client = new TcpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(end);
+                    Console.WriteLine("서버에 연결되었습니다.");
+
+                    NetworkStream stream = client.GetStream();
+
+                    _ = Task.Run(() => ReadAsync(stream));
+                    SendAsync(stream);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"오류 발생 : {e}");
+                }
+                finally
+                {
+                    Console.WriteLine("서버와의 연결을 종료합니다.");
+                    client.Close();
+                }
+            }
+
+            while (true)
+            {
+
+            }
+        }
+
+        static byte[] Packet(string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] lengthBytes = BitConverter.GetBytes(messageBytes.Length);
+            byte[] packet = new byte[4 + messageBytes.Length];
+            Buffer.BlockCopy(lengthBytes, 0, packet, 0, 4);
+            Buffer.BlockCopy(messageBytes, 0, packet, 4, messageBytes.Length);
+
+            return packet;
+        }
+
+        static async Task SendAsync(NetworkStream stream)
+        {
+            while (true)
+            {
+                string? message = Console.ReadLine();
+                var packet = Packet(message);
+                await stream.WriteAsync(packet, 0, packet.Length);
+            }
+        }
+
+        static async Task ReadAsync(NetworkStream stream)
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] length = new byte[4];
+                    int lengthCount = await stream.ReadAsync(length, 0, 4);
+                    if (lengthCount < 4)
+                        return;
+                    int dataLength = BitConverter.ToInt32(length, 0);
+                    byte[] data = new byte[dataLength];
+                    int totalRead = 0;
+                    while (totalRead < dataLength)
+                    {
+                        int byteCount = await stream.ReadAsync(data, totalRead, dataLength - totalRead);
+                        if (byteCount == 0)
+                        {
+                            return;
+                        }
+
+                        totalRead += byteCount;
+                    }
+
+                    string message = Encoding.UTF8.GetString(data);
+                    Console.WriteLine($"받은 메세지 : {message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"오류 발생 : {e}");
+                }
+            }
+            }
+    }
+}
---
+++ ServerTest/Server/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static List<TcpClient> clients = new List<TcpClient>();
+        static object _lock = new object();
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string host = Dns.GetHostName();
+                IPHostEntry ipHost = Dns.GetHostEntry(host);
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint end = new IPEndPoint(ipAddr, 7777);
+                TcpListener listener = new TcpListener(end);
+
+                listener.Start();
+                Console.WriteLine("서버가 시작되었습니다.");
+
+
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    clients.Add(client);
+                    Console.WriteLine("클라이언트가 입장하였습니다.");
+                    Task.Run(() => ClientAsync(client));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"오류 : {e}");
+
+            }
+        }
+
+        static byte[] Packet(string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] lengthBytes = BitConverter.GetBytes(messageBytes.Length);
+            byte[] packet = new byte[4 + messageBytes.Length];
+            Buffer.BlockCopy(lengthBytes, 0, packet, 0, 4);
+            Buffer.BlockCopy(messageBytes, 0, packet, 4, messageBytes.Length);
+
+            return packet;
+        }
+
+        static async Task BroadCast(string message, TcpClient sender)
+        {
+            List<TcpClient> client;
+            lock (_lock)
+            {
+                client = new List<TcpClient>(clients);
+            }
+
+            foreach (var v in client)
+            {
+                if (sender == v)
+                    continue;
+
+                var packet = Packet(message);
+                try
+                {
+                    await v.GetStream().WriteAsync(packet, 0, packet.Length);
+                }
+                catch
+                {
+                    Console.WriteLine("오류 발생 : 브로드 캐스트");
+                    clients.Remove(v);
+                    v.Close();
+                }
+            }
+        }
+
+        static async Task ClientAsync(TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+
+                    NetworkStream stream = client.GetStream();
+                    byte[] length = new byte[4];
+                    int lengthCount = await stream.ReadAsync(length, 0, 4);
+                    if (lengthCount < 4)
+                        return;
+                    int dataLength = BitConverter.ToInt32(length, 0);
+                    byte[] data = new byte[dataLength];
+                    int totalRead = 0;
+                    while (totalRead < dataLength)
+                    {
+                        int byteCount = await stream.ReadAsync(data, totalRead, dataLength - totalRead);
+                        if (byteCount == 0)
+                        {
+                            return;
+                        }
+
+                        totalRead += byteCount;
+                    }
+
+                    string message = Encoding.UTF8.GetString(data);
+                    Console.WriteLine($"받은 메세지 : {message}");
+
+                    BroadCast(message, client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"클라이언트와의 연결이 끊어졌습니다.");
+                client.Close();
+                clients.Remove(client);
+            }
+        }
+    }
+}
